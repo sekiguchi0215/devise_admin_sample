7f6f46fb0d668acf0bf88873e0c640d6169ade7f
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-16 18:00:40 +0900

    管理者画面トップページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, path: "admin"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root "homes#index"
+  end
 end
